3b832e87c89edbbe6a68a60e8f161cca9ba799ec
Oliver Athorn <oathorn@tutanota.com>
2022-08-17 08:13:35 +0100

    Initailised database and created Holidays table

---
+++ HolidayChatAgent/HolidayChatAgent.Database/Tables/Holidays.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Holidays]
+(
+	[HolidayReference] INT NOT NULL PRIMARY KEY, 
+    [HotelName] VARCHAR(60) NOT NULL, 
+    [City] VARCHAR(60) NULL, 
+    [Continent] VARCHAR(20) NOT NULL, 
+    [Country] VARCHAR(60) NOT NULL, 
+    [Category] VARCHAR(20) NOT NULL, 
+    [StarRating] INT NULL, 
+    [TempRating] VARCHAR(10) NOT NULL, 
+    [Location] VARCHAR(10) NOT NULL, 
+    [PricePerNight] MONEY NOT NULL
+)
