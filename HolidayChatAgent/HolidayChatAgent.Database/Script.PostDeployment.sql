e84b5c05545aae448ddfaedfc5c8e3abeeabf6ac
Oliver Athorn <oathorn@tutanota.com>
2022-08-17 08:30:45 +0100

    Created PostDeployment Script

---
+++ HolidayChatAgent/HolidayChatAgent.Database/Script.PostDeployment.sql
@@ -0,0 +1,17 @@
+﻿if not exists (select 1 from dbo.Holidays)
+begin
+	insert into dbo.Holidays(
+	HolidayReference,
+	HotelName,
+	City,
+	Continent,
+	Country,
+	Category,
+	StarRating,
+	TempRating,
+	Location,
+	PricePerNight)
+
+	values 
+	(1, 'Uptown', 'Bali', 'Asia', 'Indonesia', 'active', 3, 'mild', 'mountain', 120);
+end
