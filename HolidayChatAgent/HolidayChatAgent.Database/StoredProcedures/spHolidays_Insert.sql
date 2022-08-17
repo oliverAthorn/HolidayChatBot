b09e93e5b2789428b10fcca8ae5e0bd7bc3ad181
Oliver Athorn <oathorn@tutanota.com>
2022-08-17 08:20:17 +0100

    Created Insert sProc for Holidays

---
+++ HolidayChatAgent/HolidayChatAgent.Database/StoredProcedures/spHolidays_Insert.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[spHolidays_Insert]
+	@HolidayReference INT,
+	@HotelName VARCHAR(60),
+	@City VARCHAR(60),
+	@Continent VARCHAR(20),
+	@Country VARCHAR(60), 
+	@Category VARCHAR(20),
+	@StarRating INT NULL,
+	@TempRating VARCHAR(20),
+	@Location VARCHAR(10),
+	@PricePerNight MONEY 
+
+AS
+BEGIN
+	INSERT INTO dbo.Holidays (
+	HolidayReference,
+	HotelName,
+	City,
+	Continent,
+	Country,
+	Category,
+	StarRating,
+	TempRating,
+	Location,
+	PricePerNight)
+
+	VALUES (
+	@HolidayReference,
+	@HotelName,
+	@City,
+	@Continent,
+	@Country,
+	@Category,
+	@StarRating,
+	@TempRating,
+	@Location,
+	@PricePerNight);
+END
