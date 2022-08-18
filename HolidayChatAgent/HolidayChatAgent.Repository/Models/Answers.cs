d963393f1e0e1324ca10a01a649df73ea78b8865
Oliver Athorn <oathorn@tutanota.com>
2022-08-18 17:33:04 +0100

    Removed MVC

---
+++ HolidayChatAgent/HolidayChatAgent.Repository/Models/Answers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayChatAgent.Repository.Models
+{
+    public class Answers
+    {
+        public string? Answer1 { get; set; }
+    }
+}
---
+++ HolidayChatAgent/HolidayChatAgent.Repository/Models/Questions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayChatAgent.Repository.Models
+{
+    public class Questions
+    {
+        public string Question1 { get; set; } = "This is Question 1";
+
+        public string Question2 { get; set; } = "This is Question 2";
+    }
+}
