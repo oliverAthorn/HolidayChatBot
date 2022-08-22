4cb777830d91ecc5eccfcdbd3b7b52b61a4c7920
Oliver Athorn <oathorn@tutanota.com>
2022-08-22 09:41:05 +0100

    Removed MVC

--- HolidayChatAgent/HolidayChatAgent.GUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HolidayChatAgent.GUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
